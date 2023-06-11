1e2aab7d7df102d0f8c94056e604ea28c111fbbf
LuchoDev <luis.saltron2@gmail.com>
2023-06-11 16:56:26 -0500

    New sout

--- HelloGit3.java
+++ HelloGit3.java
@@ -1,5 +1,6 @@
 public class HelloGit3 {
     public static void main(String[] args) {
         System.out.println("Hello newcommer git3v3 main");
+        System.out.println("Testing to merge with main");
     }
 }
